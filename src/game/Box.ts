7a7f2617d5bfd02063010df6102ecbff2ea1669a
kid <2713786149@qq.com>
2024-04-09 01:27:55 +0800

    feat: generate random boxes

--- src/game/Box.ts
+++ src/game/Box.ts
@@ -12,6 +12,74 @@ export class Box {
     }
 }
 
+const shapeTemplate: any  = {
+    1: {
+        type: 1,
+        shape: [
+            [1, 1],
+            [1, 1],
+        ],
+    },
+    2: {
+        type: 2,
+        shape: [
+            [1, 0, 0],
+            [1, 1, 0],
+            [0, 1, 0],
+        ],
+    },
+    3: {
+        type: 4,
+        shape: [
+            [0, 0, 1],
+            [0, 1, 1],
+            [0, 1, 0],
+        ]
+    },
+    4: {
+        type: 4,
+        shape: [
+            [1, 0, 0],
+            [1, 0, 0],
+            [1, 1, 0],
+        ],
+    },
+    5: {
+        type: 5,
+        shape: [
+            [0, 0, 1],
+            [0, 0, 1],
+            [0, 1, 1],
+        ],
+    },
+    6: {
+        type: 6,
+        shape: [
+            [1, 1, 1],
+            [0, 1, 0],
+            [0, 0, 0],
+        ],
+    },
+    7: {
+        type: 7,
+        shape: [
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+        ]
+    }
+};
+
 export function createBox() {
-    return new Box();
+    const box = new Box();
+    const { shape } = randomGenerateShape();
+    box.shape = shape;
+    return box;
+}
+
+function randomGenerateShape() {
+    const max = Object.keys(shapeTemplate).length;
+    const type = Math.floor(Math.random() * max) + 1;
+    return shapeTemplate[type];
 }
