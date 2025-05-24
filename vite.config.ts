6a3f3030fdff315ffbc157d5fa0d96ab6701e7f7
kid <2713786149@qq.com>
2025-05-24 22:20:20 +0800

    feat: upgrade dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test:{ environment: "happy-dom" },
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
 })
